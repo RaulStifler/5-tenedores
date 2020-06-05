5b20f0ca971a95a1a6eb685e92e4987f62d77c27
RaulStifler <raulstifler25@gmail.com>
2020-06-05 03:11:19 -0500

    Se crea la estructura de register

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
 
 const Register = () => {
   return (
     <View>
-      <Text>Register</Text>
+      <Image
+        source={ require("../../../assets/img/5-tenedores.png") }
+        resizeMode="contain"
+        style={styles.logo}
+      />
+      <View style={styles.viewForm}>
+        <Text>Register form</Text>
+      </View>
     </View>
   )
 }
 
 export default Register
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+  viewForm: {
+    marginRight: 40,
+    marginLeft: 40,
+    
+  },
+})
 
