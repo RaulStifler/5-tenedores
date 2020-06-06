b8be70a2b75bebcd9973ea81488f31e84d7996f5
RaulStifler <raulstifler25@gmail.com>
2020-06-05 23:29:36 -0500

    se agregan los iconos en el form register

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -8,19 +8,41 @@ const RegisterForm = () => (
     <Input
       placeholder="Correo electronico"
       containerStyle={styles.inputForm}
+      rightIcon={
+        <Icon
+          type="material-community"
+          name="at"
+          iconStyle={styles.iconRight}
+        />
+      }
     />
     <Input
       placeholder="Contraseña"
       containerStyle={styles.inputForm}
       secureTextEntry
+      rightIcon={
+        <Icon
+          type="material-community"
+          name="eye-outline"
+          iconStyle={styles.iconRight}
+        />
+      }
     />
     <Input
       placeholder="Repetir contraseña"
       containerStyle={styles.inputForm}
       secureTextEntry
+      rightIcon={
+        <Icon
+          type="material-community"
+          name="eye-outline"
+          iconStyle={styles.iconRight}
+        />
+      }
     />
     <Button
       title="Unirte"
+      containerStyle={styles.buttonContainer}
       buttonStyle={styles.buttonRegister}
     />
   </View>
@@ -30,18 +52,24 @@ export default RegisterForm
 
 const styles = StyleSheet.create({
   formContainer: {
-    // flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    // marginTop: 30,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
   },
   inputForm: {
     width: '100%',
     // marginTop: 20,
   },
-  buttonRegister: {
+  buttonContainer: {
     marginTop: 20,
+    marginBottom: 20,
     width: '100%',
+  },
+  buttonRegister: {
     backgroundColor: '#00A680',
-  }
+  },
+  iconRight: {
+    color: '#C1C1C1'
+  },
 })
